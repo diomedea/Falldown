2bf188856330495d613c3e20143604a09238ecbd
diomedea <alesvill@gmail.com>
2017-05-27 17:21:32 +0200

    integral of drag until vessel stops, for the case of initialSpeed = terminalVelocity

--- Fall/Down.cs
+++ Fall/Down.cs
@@ -147,19 +147,21 @@ namespace Fall
             double Thrust, maxT, maxFF = 0;
             GetAeroStats(Ship, out Thrust, out maxT, out maxFF, out CdS);
 
-            double G, D, Tv, oldD = 0;  // gravity, air density, terminal velocity, previous distance
+            double Gg, Gz, D, Tv, oldD = 0;  // gravity at ground, gravity at altitude, air density, terminal velocity, previous distance
+            Gg = gravity(body.gravParameter, body.Radius + LS.altitude);
             do
             {  /* with altitude, compute air density and gravity, then terminalVelocity; burntime is what required for forces on craft (thrust, drag, - gravity) to stop it.
                   at terminal velocity, drag deceleration = gravity; at 0 speed, drag deceleration is nil; drag +gravity can then be averaged as 1/2 gravity;
-                  however drag follows speed^2, quadratic: the integral of (thrust + drag)/mass - gravity in time (giving speed) is ~ 10% lower then the average of (thrust/mass - gravity/2) in time
-                  therefore the equation for buntime uses the simplified average upped by 10%, rather then atttempting to recursively solve the integral */
-                G = gravity(body.gravParameter, body.Radius + LS.altitude + distance);
+                  the integral of acceleration from drag (being it parabolic with speed^2) = 1/3 Gz* Bt (if at terminal velocity) (derives from http://www.intmath.com/blog/mathematics/archimedes-and-the-area-of-a-parabolic-segment-1652)
+                  NOTE: vessel under drag and gravity accelerates towards the terminal velocity, but never reaches it! Therefore, drag deceleration != gravity and speed != terminalVelocity
+                 */
+                Gz = gravity(body.gravParameter, body.Radius + LS.altitude + distance);
                 D = density(body.GetPressure(LS.altitude + distance), body.GetTemperature(LS.altitude + distance) + atmTempOffset(), 
                     PhysicsGlobals.IdealGasConstant / body.atmosphereMolarMass);
-                Tv = Math.Sqrt(2 * (Ship.totalMass-maxFF*Bt) * G / (D * CdS));
-                Bt = 1.1 * Tv / (maxT/(Ship.totalMass- maxFF*Bt/2)- gravity(body.gravParameter, body.Radius + LS.altitude)/2);
+                Tv = Math.Sqrt(2 * (Ship.totalMass-maxFF*Bt) * Gg / (D * CdS));
+                Bt = Tv / (maxT/2*(1/Ship.totalMass +1/(Ship.totalMass- maxFF*Bt)) + Gz/3 - (Gz+Gg)/2);
                 oldD = distance;
-                distance = 0.5 * (maxT - G) * Bt * Bt;  // note: only valid for vertical descents with thrust oriented against gravity; would require vector sum of maxT, G to compute vector distance
+                distance = 0.5 * (maxT - Gg) * Bt * Bt;  // note: only valid for vertical descents with thrust oriented against gravity; would require vector sum of maxT, G to compute vector distance
             } while (Math.Abs(distance-oldD) > 0.1);
         }
 
